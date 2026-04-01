6aeb7870465b877c8bbff65143ead06ca56d5a4a
Jason Abraham Rajan <jasonrajan20@gmail.com>
2026-04-01 12:16:16 +0530

    [Jason] feat: add update Terisa salary using JDBC

--- src/main/java/com/java/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/java/bridgelabz/EmployeePayrollService.java
@@ -2,7 +2,6 @@ package com.java.bridgelabz;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class EmployeePayrollService {
@@ -15,24 +14,17 @@ public class EmployeePayrollService {
 
         try {
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
             Connection connection =
                     DriverManager.getConnection(jdbcURL, username, password);
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet =
-                    statement.executeQuery("select * from employee_payroll");
+            int rowsAffected =
+                    statement.executeUpdate(
+                            "update employee_payroll set salary = 3000000 where name = 'Terisa'"
+                    );
 
-            while(resultSet.next()) {
-
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                double salary = resultSet.getDouble("salary");
-
-                System.out.println(id + " " + name + " " + salary);
-            }
+            System.out.println("Rows Updated: " + rowsAffected);
 
         } catch(Exception e) {
             e.printStackTrace();
